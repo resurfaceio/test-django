fa1379a51fc5826ebc229880f419111f6540f2f4
Anyesh <sthacruz@gmail.com>
2021-02-14 12:25:00 +0545

    Add pong views

--- django_integration_test/views.py
+++ django_integration_test/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from graphene_django.views import GraphQLView
 
 
 class PrivateGraphQLView(LoginRequiredMixin, GraphQLView):
     pass
+
+
+def pong(request):
+    return JsonResponse({"msg": "pong"})

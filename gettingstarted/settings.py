e4f5f444645725cf2aa24ac1b90b8a7e5405e8fe
Rob Dickinson <robfromboulder@gmail.com>
2021-03-02 22:49:02 -0700

    Revert last change

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "usagelogger.middlewares.HttpLoggerForDjango",
+    "usagelogger.django.HttpLoggerForDjango",
 ]
 
 USAGELOGGER = {
